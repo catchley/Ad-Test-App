f6e880f52dc978cef4293e4637dfcffaef9af599
Chris Atchley <steve.chris.atchley@gmail.com>
2015-06-20 22:36:27 -0400

    Added all manifest files and dependencies.

---
+++ android/src/atchley/chris/dailyjoke/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package atchley.chris.dailyjoke.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import atchley.chris.dailyjoke.DailyJoke;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DailyJoke(), config);
+	}
+}
---
+++ desktop/src/atchley/chris/dailyjoke/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package atchley.chris.dailyjoke.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import atchley.chris.dailyjoke.DailyJoke;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DailyJoke(), config);
+	}
+}
